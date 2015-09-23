10de2b8eb9b185838b944763c50f986f81784252
tsukumo <tsu.complete@gmail.com>
2015-09-22 22:46:02 -0400

    日本語文書化を追加

---
+++ dox/jp.hxx
@@ -0,0 +1,34 @@
+
+/**
+ * \namespace slic
+ * \brief SLIC関連構造体とアルゴリズムのためのコンテナ
+ */
+
+/**
+ * \class Point
+ * \memberof slic
+ * \brief 内部の画素情報の親クラス
+ */
+
+/**
+ * \var label
+ * \public
+ * \memberof Point
+ * \brief このポイントのスーパーピクセル・シード番号
+ */
+
+/**
+ * \fn Point
+ * \public
+ * \memberof Point
+ * \brief 付属のラベルを持つ新しいポイントを作成
+ * \param label {unsigned int : 0} 最初のラベル
+ */
+
+/**
+ * \fn ~Point
+ * \public
+ * \memberof Point
+ * \brief 継承のための仮想デストラクタ
+ */
+
