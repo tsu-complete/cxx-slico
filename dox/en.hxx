9d681be0806ad84269eb23ddcef7a24f3f3bd3f7
tsukumo <tsu.complete@gmail.com>
2015-09-22 22:11:00 -0400

    Initialize doxygen documentation
    Currently only en support

---
+++ dox/en.hxx
@@ -0,0 +1,34 @@
+
+/**
+ * \namespace slic
+ * \brief container for slic related structs and algorightms
+ */
+
+/**
+ * \class Point
+ * \memberof slic
+ * \brief base class for internal pixel information
+ */
+
+/**
+ * \var label
+ * \public
+ * \memberof Point
+ * \brief denote what superpixel seed this point belongs to or is
+ */
+
+/**
+ * \fn Point
+ * \public
+ * \memberof Point
+ * \brief create a new point with the supplied label
+ * \param label {unsigned int : 0} initial label of this point
+ */
+
+/**
+ * \fn ~Point
+ * \public
+ * \memberof Point
+ * \brief virtual destructor for inheritance
+ */
+
