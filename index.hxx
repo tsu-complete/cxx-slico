a11ec73ea91d57726005e607ff849d2455c692df
tsukumo <tsu.complete@gmail.com>
2015-09-22 22:11:38 -0400

    Update index to match documentation

--- index.hxx
+++ index.hxx
@@ -4,10 +4,21 @@
 
 #include <limits>
 
-const double MAX_DOUBLE = std::numeric_limits<double>::max();
-
 namespace slic
 {
+    const double MAX_DOUBLE = std::numeric_limits<double>::max();
+
+    class Point
+    {
+    public:
+        unsigned int label;
+
+        Point ( unsigned int _label = 0 )
+            : label(_label)
+        { }
+
+        virtual ~Point ( ) { }
+    };
 
 };
 
